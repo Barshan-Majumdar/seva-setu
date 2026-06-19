6313110f342e02da0da096119069e62b5eca655c
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-06-19 22:13:04 +0530

    Removed alret

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,7 +50,7 @@ function MainContent() {
 
   return (
     <>
-      <ServerMaintenanceAlert />
+      {/* <ServerMaintenanceAlert /> */}
       <AuthTokenBridge />
       <RoleSync onReady={handleSyncReady} />
 
